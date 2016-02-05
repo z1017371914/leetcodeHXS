5ca5266037d88834c965b4ded24bb39c5b342092
z1017371914 <1017371914@qq.com>
2016-02-05 20:16:47 +0800

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,26 +1,46 @@
-class Solution {  ////想法很奇妙，分别把从两端开始求，然后从两边那个较小的边开始移动遇到比较大的边求一下面积
+class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int low =0;
-        int high = height.size()-1;
-        int res = 0;
-        while (low<high) {
-            res = max(res,min(height[low],height[high])*(high-low));
-            if (height[low]<height[high]) {
-                int temp = low;
-                while (temp<high&&height[temp]<=height[low]) {
-                    temp++;
-                }
-                low = temp;
-            }
-            else{
-                int temp  = high;
-                while (temp>low&&height[temp]<=height[high]) {
-                    temp--;
-                }
-                high = temp;
-            }
+    //思想 1.判断p的第二个字符是否是*,如果不是* 递归判断s+1 与 p+1
+    //2.如果是* 如果s与p的第一个字符不相等，则判断 s月p+2
+      // 如果第一个数相等也要分两种情况(1)*算0次 则判断s与 p+2    (2) 判断s后面的子串与p 直到 s第一个字符与p的第一个字符不相等
+    bool isMatch(string s, string p) {
+        if(s.size()==0)
+            return (p.size()==0||(p.size()>1&&p[1]=='*'&&isMatch(s,p.substr(2))));
+        if (p.size()==0) {
+            return s.size()==0;
         }
-        return res;
+        //当p的长度为1时
+        if (p.size()==1) {
+            return p==s||(p=="."&&s.size()==1);
+            
+            return false;
+        }
+        //当p的长度不为1时
+        if(p[1]!='*'){
+            if (s[0]==p[0]||p[0]=='.') {
+                return isMatch(s.substr(1), p.substr(1));
+            }
+            return  false;
+            
+        }
+        else{
+            //1 .如果第一个字符不相等
+            if (s[0]!=p[0]&&p[0]!='.') {
+                return isMatch(s,p.substr(2));
+            }
+            //2.如果第一个字符相等且p第二个字符是*
+            while (s.size()>0&&(s[0]==p[0]||p[0]=='.')) {
+                ///出现了0次
+                if (isMatch(s, p.substr(2))) {
+                    return true;
+                }
+                //s后面的截取然后递归
+                s=s.substr(1);
+            }
+                return isMatch(s, p.substr(2));
+            
+            
+        }
+        
     }
 };
